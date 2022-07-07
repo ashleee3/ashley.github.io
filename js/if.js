2182bf5407c8702d347d3cd1afbd026da1bbc553
ashleee3 <ashley.omondi99@gmail.com>
2022-07-07 13:52:44 +0300

    my first commit

---
+++ js/if.js
@@ -0,0 +1,14 @@
+for (let index = 0; index <= 20; index++) {
+    console.log(index);
+    
+}
+for (let index = 0; index <=10; index++) {
+    console.log(index, "tupo site");
+    
+}
+const school={
+    teachers:70,
+    students:1500,
+    category:"county school"
+    
+}
---
+++ js/switch.js
@@ -0,0 +1,13 @@
+
+
+switch ("Anna") {
+    case "Joe":
+        console.log("Joe has been found");
+        break;
+
+     case "Anna":
+         console.log("Anna has been found");
+
+    default:
+        break;
+}
